37b616245cc61a040f9361c539bc3f8ffd45d180
KevinXie <lylwo317@gmail.com>
2021-02-19 13:52:43 +0800

    add queue

---
+++ src/com/kevin/datastructures/queue/PriorityQueue.java
@@ -0,0 +1,44 @@
+package com.kevin.datastructures.queue;
+
+import com.kevin.datastructures.heap.BinaryHeap;
+import com.kevin.datastructures.heap.Heap;
+import com.kevin.datastructures.list.LinkedList;
+import com.kevin.datastructures.list.List;
+
+import java.util.Comparator;
+
+public class PriorityQueue<E> {
+	private Heap<E> heap;
+
+	public PriorityQueue() {
+		this(null);
+	}
+
+	public PriorityQueue(Comparator<E> comparator) {
+		heap = new BinaryHeap<>(comparator);
+	}
+
+	public int size() {
+		return heap.size();
+	}
+
+	public boolean isEmpty() {
+		return heap.isEmpty();
+	}
+	
+	public void clear() {
+		heap.clear();
+	}
+
+	public void enQueue(E element) {
+		heap.add(element);
+	}
+
+	public E deQueue() {
+		return heap.remove();
+	}
+
+	public E front() {
+		return heap.get();
+	}
+}
