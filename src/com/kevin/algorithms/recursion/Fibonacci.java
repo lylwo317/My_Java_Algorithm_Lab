9ecb44a8917cb2dbe79b230b69ad995ef459334f
Kevin Xie <lylwo317@gmail.com>
2021-03-07 14:09:21 +0800

    add comment to the fibonacci sequence

--- src/com/kevin/algorithms/recursion/Fibonacci.java
+++ src/com/kevin/algorithms/recursion/Fibonacci.java
@@ -1,33 +1,50 @@
 package com.kevin.algorithms.recursion;
 
 
+/**
+ * 斐波那契数列
+ *
+ * 0 1 1 2 3 5 8 ...
+ */
 public class Fibonacci {
 
-    public int fib(int n){
+    public int fib1(int n){//O(2^n)  T(n) = T(n-1) + T(n-2) + O(1)
         if (n <= 1) {
             return n;
         }
-        return fib(n - 1) + fib(n - 2);
+        return fib1(n - 1) + fib1(n - 2);
     }
 
-    public int fib1(int n){
+    public int fib2(int n){
         if (n <= 1) {
             return n;
         }
         int[] array = new int[n + 1];
         array[1] = 1;
         array[2] = 1;
-        return fib1(n, array);
+        return fib2(n, array);
     }
 
-    public int fib1(int n, int[] array) {
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     * @param n
+     * @return
+     */
+    public int fib2(int n, int[] array) {
         if (array[n] == 0) {
-            array[n] = fib1(n - 1, array) + fib1(n - 2, array);
+            array[n] = fib2(n - 1, array) + fib2(n - 2, array);
         }
         return array[n];
     }
 
-    public int fib2(int n) {
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * @param n
+     * @return
+     */
+    public int fib3(int n) {
         int[] array = new int[2];
 //        array[0] = 0;
         array[1] = 1;
@@ -37,7 +54,13 @@ public class Fibonacci {
         return array[n%2];
     }
 
-    public int fib3(int n) {
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * @param n
+     * @return
+     */
+    public int fib4(int n) {
         if (n <= 1) {
             return n;
         }
@@ -53,7 +76,7 @@ public class Fibonacci {
 
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
-        test(fibonacci::fib1);
+        test(fibonacci::fib2);
     }
 
     private static void test(Callable callable) {
