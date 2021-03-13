af535f949e10ea609e7cbec9568039fcc2bbd059
KevinXie <lylwo317@gmail.com>
2021-03-13 09:05:05 +0800

    use dynamic programming to solve CoinChange problem

--- src/com/kevin/algorithms/dp/CoinChange.java
+++ src/com/kevin/algorithms/dp/CoinChange.java
@@ -1,8 +1,66 @@
 package com.kevin.algorithms.dp;
 
+/**
+ * 找零钱问题
+ */
 public class CoinChange {
     public static void main(String[] args) {
-        System.out.println(coins2(41, new int[] {1, 5, 25, 20}));
+        System.out.println(coins3(41, new int[] {1, 5, 25, 20}));
+    }
+
+    /**
+     * 动态规划/递推，自底向上调用
+     *
+     * 状态定义：
+     * dp[i] 表示筹够i块钱，需要的最小硬币数
+     *
+     * 初始状态：
+     * dp[face[0]] = 1, dp[face[1]] = 1 ...
+     *
+     * 状态转移方程：
+     * dp[i] = min(dp[i-face[0]], dp[i-face[1]], ...) + 1//类似走楼梯
+     *
+     * @param money
+     * @param faces
+     * @return
+     */
+    private static int coins3(int money, int[] faces) {
+        int[] selected = new int[money + 1];
+        int[] dp = new int[money + 1];
+        if (money < 1) {
+            return 0;
+        }
+        //初始
+        for (int face : faces) {
+            dp[face] = 1;
+        }
+
+        for (int i = 1; i <= money; i++) {
+            int min = Integer.MAX_VALUE;
+            int selectedFace = Integer.MAX_VALUE;
+            for (int face : faces) {
+                if (i - face < 0) {
+                    continue;
+                }
+                if (dp[i - face] < min) {
+                    min = dp[i - face];
+                    selectedFace = face;
+                }
+//                min = Math.min(dp[i - face], min);
+            }
+            if (min != Integer.MAX_VALUE) {//说明有可兑换的硬币
+                dp[i] = min + 1;
+                selected[i] = selectedFace;
+            }
+        }
+        int m = money;
+        System.out.println(money + " 需要兑换的硬币:");
+        while (m > 0) {
+            System.out.println(selected[m]);//筹够m块钱的最后一枚硬币的面额
+            m -= selected[m];
+        }
+        System.out.println();
+        return dp[money];
     }
 
     private static int coins2(int money, int[] faces) {
@@ -69,6 +127,7 @@ public class CoinChange {
 
     /**
      * 暴力递归（自顶向下的调用，存在重叠子问题，所以效率比较差）
+     * 复杂度：类似斐波那契数
      * @param money
      * @param faces
      * @return
