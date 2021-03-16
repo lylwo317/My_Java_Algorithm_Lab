4e9d25a96bd5e5ad2998376c1ac7d00dc2e9a143
KevinXie <lylwo317@gmail.com>
2021-03-16 14:26:39 +0800

    add comment for KMP

--- src/com/kevin/algorithms/sequence/KMP.java
+++ src/com/kevin/algorithms/sequence/KMP.java
@@ -1,8 +1,53 @@
 package com.kevin.algorithms.sequence;
 
-import java.util.Arrays;
-
 /**
+ * 核心思想：
+ * 对已经匹配过的字符，如无必要，不要再次匹配
+ *
+ * 1. 模板字符串中包含公共前后缀
+ *           ↓
+ * ABCDGHABCDGHJSJGDSJGDSHDJK
+ * ABCDGHABCDIO
+ *           ↑
+ *
+ *           ↓
+ * ABCDGHABCDGHJSJGDSJGDSHDJK
+ *       ABCDGHABCDIO
+ *           ↑
+ *
+ *             ↓
+ * ABCDGHABCDGHJSJGDSJGDSHDJK
+ *             ABCDGHABCDIO
+ *             ↑
+ *
+ *              ↓
+ * ABCDGHABCDGHJSJGDSJGDSHDJK
+ *              ABCDGHABCDIO
+ *              ↑
+ *
+ *               ↓
+ * ABCDGHABCDGHJSJGDSJGDSHDJK
+ *               ABCDGHABCDIO
+ *               ↑
+ *
+ * 2. 模板字符串中不包含公共前后缀
+ *     ↓
+ * ABCDGHABCDGHJSJGDSJGDSHDJK
+ * ABCDEFGHIO
+ *     ↑
+ *
+ *     ↓
+ * ABCDGHABCDGHJSJGDSJGDSHDJK
+ *     ABCDEFGHIO
+ *     ↑
+ *
+ *      ↓
+ * ABCDGHABCDGHJSJGDSJGDSHDJK
+ *      ABCDEFGHIO
+ *      ↑
+ *
+ * 由上面两种情况。我们可以知道，处理好包含公共前后缀的情况就不用每次都重新比较
+ *
  * Created by: kevin
  * Date: 2021-03-14
  */
@@ -11,7 +56,7 @@ public class KMP {
      * 配合 https://blog.csdn.net/yyzsir/article/details/89462339  来理解
      *
      * p    = [ D,  B,  C,  D,  E,  D,  B,  C,  E,  A,  B,  C,  D,  E,  A,  B,  C,  D,  F,  B,  G]
-     * next = [-1,  0,  0,  0,  1,  0,  1,  2,  3,  0,  0,  0,  0,  1,  0,  0,  0,  0,  1,  0,  0]
+     * next = [-1,  0,  0,  0,  1,  0,  1,  2,  3,  0,  0,  0,  0,  1,  0,  0,  0,  0,  1,  0,  0]//标识pIndex位置
      *
      * next 数组的值是除当前字符外（注意不包括当前字符）的公共真前后缀最长长度，相当于将表中公共前后缀最长长度全部右移一位，第一个值赋为-1。
      * @param pattern
@@ -21,11 +66,11 @@ public class KMP {
         char[] p = pattern.toCharArray();
         int[] next = new int[p.length];
 
-        next[0] = -1;
         int j = 0;
-        int k = -1;//next数组值
+        next[j] = -1;
+        int k = next[j];//next数组值
         while (j < p.length - 1) {//整体右移1位，所以不需要计算p.length - 1对应的next
-            if (k < 0 || p[j] == p[k]) {
+            if (k == -1 || p[j] == p[k]) {//找不到更短的公共前后缀 或者 已p[j]结尾包含公共前后缀（p[j] == p[k]）
                 next[++j] = ++k;
             } else {
                 k = next[k];//往前找更短的公共前后缀
@@ -51,16 +96,16 @@ public class KMP {
         int j = 0;
         int k = -1;//next数组值
         while (j < p.length - 1) {//整体右移1位，所以不需要计算p.length - 1对应的next
-            if (k < 0 || p[j] == p[k]) {
+            if (k == -1 || p[j] == p[k]) {//找不到更短的公共前后缀 或者 已p[j]结尾包含公共前后缀（p[j] == p[k]）
 //                next[++j] = ++k;
 //                ++j;
 //                ++k;
                 if (p[++j] == p[++k]) {
-                    // 说明到时候匹配p[j]的时候。
-                    // 如果不相等，通过next数组，挪动到p[k]也是不相等的。所以完全可以缩减到p[next[k]]
-                    next[j] = next[k];
+                    // 说明到时候匹配p[++j]的时候。
+                    // 通过next数组，挪动到p[k]也是不相等的。所以完全可以缩减到p[next[k]]
+                    next[j] = next[k];//原来是next[j] = k，现在多往前一次也就是next[j] = next[k]
                 } else {
-                    next[j] = k;//优化k<0的情况，
+                    next[j] = k;//无法缩减，按照原来的方式处理
                 }
             } else {
                 k = next[k];//往前找更短的公共前后缀
@@ -134,7 +179,7 @@ public class KMP {
             } else {
 //                tIndex = tIndex - pIndex + 1;
 //                pIndex = 0;
-                pIndex = next[pIndex];
+                pIndex = next[pIndex];//优化回溯的位置，减少重复比较次数
             }
         }
 
