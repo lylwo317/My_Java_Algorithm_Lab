99d706abb419cca5c3ae2f151680f661d75b90ca
KevinXie <lylwo317@gmail.com>
2021-04-15 10:06:45 +0800

    solution of _面试题_01_09_字符串轮转

---
+++ src/leetcode/editor/cn/_面试题 01.09_字符串轮转.java
@@ -0,0 +1,51 @@
+package leetcode.editor.cn;
+//字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成（比如，waterbottle是erbottlewat旋转后的字符串）。 
+//
+// 示例1: 
+//
+//  输入：s1 = "waterbottle", s2 = "erbottlewat"
+// 输出：True
+// 
+//
+// 示例2: 
+//
+//  输入：s1 = "aa", s2 = "aba"
+// 输出：False
+// 
+//
+// 
+// 
+//
+// 提示： 
+//
+// 
+// 字符串长度在[0, 100000]范围内。 
+// 
+//
+// 说明: 
+//
+// 
+// 你能只调用一次检查子串的方法吗？ 
+// 
+// Related Topics 字符串 
+// 👍 66 👎 0
+
+//https://leetcode-cn.com/problems/string-rotation-lcci/
+
+class _面试题_01_09_字符串轮转{
+    Solution solution = new Solution();
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isFlipedString(String s1, String s2) {
+        if (s1 == null || s2 == null) {
+            return false;
+        }
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        return (s1 + s1).contains(s2);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
