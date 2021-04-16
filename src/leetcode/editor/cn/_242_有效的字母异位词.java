a8f41774a624b75e8d19df3d64ef84fafe3b3684
KevinXie <lylwo317@gmail.com>
2021-04-16 13:57:09 +0800

    solution of _242_有效的字母异位词

---
+++ src/leetcode/editor/cn/_242_有效的字母异位词.java
@@ -0,0 +1,52 @@
+package leetcode.editor.cn;
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。 
+//
+// 示例 1: 
+//
+// 输入: s = "anagram", t = "nagaram"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: s = "rat", t = "car"
+//输出: false 
+//
+// 说明: 
+//你可以假设字符串只包含小写字母。 
+//
+// 进阶: 
+//如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？ 
+// Related Topics 排序 哈希表 
+// 👍 370 👎 0
+
+//https://leetcode-cn.com/problems/valid-anagram/
+
+class _242_有效的字母异位词{
+    Solution solution = new Solution();
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isAnagram(String s1, String s2) {
+        if (s1 == null || s2 == null) {
+            return false;
+        }
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        int length = s1.length();
+        for (int i = 0; i < length; i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
