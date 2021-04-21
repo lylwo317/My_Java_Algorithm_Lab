6a3fc614907bc1fe6e0f3598a955564e8369b102
Kevin Xie <lylwo317@gmail.com>
2021-04-21 22:22:41 +0800

    optimize the solution of _70_爬楼梯

--- src/leetcode/editor/cn/_70_爬楼梯.java
+++ src/leetcode/editor/cn/_70_爬楼梯.java
@@ -37,16 +37,13 @@ class Solution {
      * @return
      */
     public int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
+        int[] dp = new int[2];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i & 1] = dp[(i - 1) & 1] + dp[(i - 2) & 1];
         }
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
+        return dp[(n - 1) & 1];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
