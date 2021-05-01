8565f13111a52b216c6fb33708d4f0dd9ccf1cd1
KevinXie <lylwo317@gmail.com>
2021-05-01 12:16:04 +0800

    solution of _690_员工的重要性

---
+++ src/com/kevin/leetcode/Employee.java
@@ -0,0 +1,13 @@
+package com.kevin.leetcode;
+
+import java.util.List;
+
+/**
+ * Created by: kevin
+ * Date: 2021-05-01
+ */
+public class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
