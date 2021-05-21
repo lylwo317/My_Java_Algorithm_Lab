c697429fe488d42459aa75cf11185b1add22e326
KevinXie <lylwo317@gmail.com>
2021-05-21 10:10:15 +0800

    solution of _692_前K个高频单词

---
+++ src/leetcode/editor/cn/_692_前K个高频单词.java
@@ -0,0 +1,99 @@
+package leetcode.editor.cn;
+//给一非空的单词列表，返回前 k 个出现次数最多的单词。 
+//
+// 返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率，按字母顺序排序。 
+//
+// 示例 1： 
+//
+// 
+//输入: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+//输出: ["i", "love"]
+//解析: "i" 和 "love" 为出现次数最多的两个单词，均为2次。
+//    注意，按字母顺序 "i" 在 "love" 之前。
+// 
+//
+// 
+//
+// 示例 2： 
+//
+// 
+//输入: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k
+// = 4
+//输出: ["the", "is", "sunny", "day"]
+//解析: "the", "is", "sunny" 和 "day" 是出现次数最多的四个单词，
+//    出现次数依次为 4, 3, 2 和 1 次。
+// 
+//
+// 
+//
+// 注意： 
+//
+// 
+// 假定 k 总为有效值， 1 ≤ k ≤ 集合元素数。 
+// 输入的单词均由小写字母组成。 
+// 
+//
+// 
+//
+// 扩展练习： 
+//
+// 
+// 尝试以 O(n log k) 时间复杂度和 O(n) 空间复杂度解决。 
+// 
+// Related Topics 堆 字典树 哈希表 
+// 👍 310 👎 0
+
+//https://leetcode-cn.com/problems/top-k-frequent-words/
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+class _692{
+    Solution solution = new Solution();
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    /**
+     * topK的问题——堆，优先队列
+     * @param words
+     * @param k
+     * @return
+     */
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<String, Integer>> p = new PriorityQueue<>(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2) {
+                return entry1.getValue().equals(entry2.getValue()) ? entry2.getKey().compareTo(entry1.getKey()) : entry1.getValue() - entry2.getValue();
+            }
+        });
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            p.offer(entry);
+            if (p.size() > k) {
+                p.poll();
+            }
+        }
+
+        List<String> result = new ArrayList<>();
+        while (!p.isEmpty()) {
+            result.add(p.poll().getKey());
+        }
+        Collections.reverse(result);
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    public static void main(String[] args) {
+        _692 problem = new _692();
+    }
+}
